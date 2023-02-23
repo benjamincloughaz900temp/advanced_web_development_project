af5fbb12e96fb50b3d9e078266201a83b1bf82b8
Benjamin <benjaminclough1004@gmail.com>
2023-02-23 19:17:18 +0000

    Type Hinted a function in MySQL_utilities

--- utilities/MySQL_utilities.py
+++ utilities/MySQL_utilities.py
@@ -1,3 +1,4 @@
 
-def get_set_info_from_workout(username: str, workout_date: str, set_number: int):
+def get_set_info_from_workout(username: str, workout_date: str, set_number: int) -> \
+        dict[str: str, str: int, str: float]:
     pass
